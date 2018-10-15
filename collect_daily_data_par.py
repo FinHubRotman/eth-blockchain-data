a337e0839af4c07af2cf1b746c84181b79c185bb
FinHubRotman <jeff.li@rotman.utoronto.ca>
2018-10-15 17:02:30 -0400

    A script that collects daily data from the ETH Blockchain This version parallizes on dates.

--- collect_daily_data_par.py
+++ collect_daily_data_par.py
@@ -1,4 +1,4 @@
-def andreas_data(final_data):
+def agg_data(final_data):
     total_tx = len(final_data)
     total_gas = sum([x['gasUsed'] for x in final_data])
     gw_gasprice = sum([x['gasPrice']*(x['gasUsed']/total_gas) for x in final_data])
@@ -8,34 +8,35 @@ def andreas_data(final_data):
 def rm_inv_filename(filename):
     return re.sub('[<>:"\\|?*]', '', filename) 
 
-def write_token_data(i, date, name, sym, addr):
-    filename = "./andreas_daily_data/" + rm_inv_filename(name) + "-" + rm_inv_filename(sym) + ".csv"
-    # check if we already have said date written on the file
-    already_written = False
-    with open(filename, 'r') as f:
-        reader = csv.reader(f)
-        reader_list = list(reader)
-        file_dates = [x[0] for x in reader_list]
-        try:
-            if date.strftime('%Y-%m-%d') in file_dates:
-                already_written = True
-        except IndexError:
-            pass
-    if not already_written:
-        with open(filename, 'a', newline="") as f:
-            writer = csv.DictWriter(f, fieldnames=['date', 'total_tx', 'total_gas', 'gw_gasprice', 'gw_mean_gasprice'])
-            blocks = Blocks(addr)
-            if i < 100:
-                date_data = blocks.data_on_date(date)
-                date = date.strftime('%Y-%m-%d')
-                data_with_date = {**{'date':date}, **andreas_data(date_data)}
-                writer.writerow(data_with_date)
-                print(name, data_with_date)
-            else:
-                date_data = blocks.par_data_on_date(date)
-                date = date.strftime('%Y-%m-%d')
-                data_with_date = {**{'date':date}, **andreas_data(date_data)}
-                writer.writerow(data_with_date)
+def write_token_data(start_date, date, token_list):
+    for name, sym, addr in token_list:
+        filename = "./andreas_daily_data/" + rm_inv_filename(name) + "-" + rm_inv_filename(sym) + ".csv"
+        # check if we already have said date written on the file
+        already_written = False
+        with open(filename, 'r') as f:
+            reader = csv.reader(f)
+            reader_list = list(reader)
+            file_dates = [x[0] for x in reader_list]
+            try:
+                if date.strftime('%Y-%m-%d') in file_dates:
+                    already_written = True
+            except IndexError:
+                pass
+        if not already_written:
+            with open(filename, 'a', newline="") as f:
+                writer = csv.DictWriter(f, fieldnames=['date', 'total_tx', 'total_gas', 'gw_gasprice', 'gw_mean_gasprice'])
+                blocks = Blocks(addr)
+                if (start_date - date).days < 100: # if we're 100
+                    date_data = blocks.data_on_date(date)
+                    date = date.strftime('%Y-%m-%d')
+                    data_with_date = {**{'date':date}, **agg_data(date_data)}
+                    writer.writerow(data_with_date)
+                    print(name, data_with_date)
+                else:
+                    date_data = blocks.par_data_on_date(date)
+                    date = date.strftime('%Y-%m-%d')
+                    data_with_date = {**{'date':date}, **agg_data(date_data)}
+                    writer.writerow(data_with_date)
 
 if __name__ == '__main__':
     import datetime
@@ -73,19 +74,18 @@ if __name__ == '__main__':
                 writer=csv.DictWriter(f, fieldnames=['date', 'total_tx', 'total_gas', 'gw_gasprice', 'gw_mean_gasprice'])
                 writer.writeheader()
 
-    date = datetime.datetime(2018,10,1)
-    for i in list(range(273))[:]: # go backwards in time
-        date = datetime.datetime(2018,10,1)
-        date = date + datetime.timedelta(days=-1*i)
-        fix_i = functools.partial(write_token_data, i)
-        fix_date = functools.partial(fix_i, date)
-        procs = []
-        for some_list in new_lists:
-            t = threading.Thread(target=fix_date, args=(*some_list,))
-            procs.append(t)
+    start_date = datetime.datetime(2018,10,1)
+    # for i in list(range(273))[:]: # go backwards in time
+    dates = [start_date + datetime.timedelta(days=-1*i) for i in range(273)]
+    fix_start = functools.partial(write_token_data, start_date)
+    procs = []
+    for date in dates:
+        fix_date = functools.partial(fix_start, date)
+        t = threading.Thread(target=fix_start, args=(new_lists,))
+        procs.append(t)
 
-        for proc in procs:
-            proc.start()
+    for proc in procs:
+        proc.start()
 
-        for proc in procs:
-            proc.join()
+    for proc in procs:
+        proc.join()
